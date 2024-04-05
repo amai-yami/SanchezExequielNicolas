d14da47d5c461a739bf04c1b001e3d584ea32bf0
exequiel sanchez <exe.sanchez18@gmail.com>
2024-04-04 21:33:21 -0300

    cambios

--- t1/funciones/funciones.js
+++ t1/funciones/funciones.js
@@ -1,1 +1,26 @@
 alert("hola mundo");
+function saludar(nombre){
+
+    var nombre= "javier";
+    
+    alert("hola " + nombre);
+}
+
+
+    function result(suma){
+
+     var suma = 5 + 6;
+     alert("el resultado es: "+suma);
+
+    }
+      
+
+   
+    //condicional if para ver menor
+    
+//operadores logicos y numericos
+
+
+//fetch revisar de javascrip  rick and morty
+//GET  https://rickandmortyapi.com/api/character
+//PETICIONES HTTP JSON Y FUNCION JAVASCRIP FETCH
